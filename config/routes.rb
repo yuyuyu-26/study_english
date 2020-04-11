a1163c3e01ad704247a2d6f9d818e763648c1c96
ゆゆゆ <yu.yu.yu.bnpg@gmail.com>
2020-04-11 13:47:17 +0000

    create basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
  
 
+  get 'sessions/new'
+
   root 'pages#home'
   get "pages/home"
   get  '/help',    to: 'static_pages#help'
@@ -8,6 +10,9 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   
   resources :users
 
