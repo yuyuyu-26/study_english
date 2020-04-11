ad36433778dd86f2c5271d85438b9b086a50c630
ゆゆゆ <yu.yu.yu.bnpg@gmail.com>
2020-04-11 04:55:20 +0000

    basic homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+ 
+
   root 'pages#home'
+  get "pages/home"
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
