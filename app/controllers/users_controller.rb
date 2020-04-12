b3e99f314465baf7ea0f10bc24e5e8f24aee6bcf
ゆゆゆ <yu.yu.yu.bnpg@gmail.com>
2020-04-12 03:55:03 +0000

    finish users edit update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -12,9 +15,23 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "英単語学習アプリへようこそ！"
-      redirect_to @user
+      redirect_back_or user
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "プロフィールが更新されました"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
   
@@ -23,5 +40,22 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation, :image)
+    end
+    
+    # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
+    
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
